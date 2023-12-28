45175c60e29fc02463cb31052aa7f7fd6f4b1b46
RealEragon <92983632+RealEragon@users.noreply.github.com>
2023-12-29 00:03:48 +0300

    Added this functions: 1. Validation of data during registration 2. User registrations 3. User account edit 4. Log into account 5. Attempt to access the account editing page by an unlogged-in user 6. Deleting account

--- actually-best-shop-app/app/controllers/application_controller.rb
+++ actually-best-shop-app/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+    end
+
+    def after_sign_in_path_for(resource)
+        user_profile_path
+    end
+
+    end
+    def after_update_path_for(resource)
+        edit_user_registration_path(resource)
+    end
+
--- actually-best-shop-app/config/routes.rb
+++ actually-best-shop-app/config/routes.rb
@@ -1,10 +1,13 @@
+# config/routes.rb
+
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'profiles/show'
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+
+  get 'profile', to: 'profiles#show', as: 'user_profile'
+  get 'up' => 'rails/health#show', as: :rails_health_check
 end
